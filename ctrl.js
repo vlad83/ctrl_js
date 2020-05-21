b5570acfbc2ec67612b365b2f65ed50810cadc41
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2020-05-21 16:24:00 +0200

    Dropped NodeJS http server. Attached Web Page is tested with lighttpd. Introduced index.html as entry point for Home Automation Web Page. Added separate page (lamp_ctrl.html) dedicated to light control. Added icons authored and released for free by Roman Malashkov (thanks Roman!).

--- ctrl.js
+++ ctrl.js
@@ -1,13 +1,13 @@
 const BROKER_ENDPOINT = "tcp://127.0.0.1:6060"
 
-var http = require('http').createServer(handler);
-var fs = require('fs');
-var socket_io = require('socket.io')(http);
+var socket_io = require('socket.io')(8090);
 
 const zmq = require("zeromq")
 
 async function updateDevice(id, mode, r, g, b)
 {
+    console.log("updateDevice", id, mode, r, g, b);
+
     zmq_socket = new zmq.Request
     zmq_socket.connect(BROKER_ENDPOINT);
     await zmq_socket.send(
@@ -43,37 +43,18 @@ async function updateDevice(id, mode, r, g, b)
     }
 }
 
-function handler(request, response)
-{
-    fs.readFile(
-        __dirname + '/public/rgb_ctrl.html',
-        function(err, fileData)
-        {
-            if (err)
-            {
-                console.log(err);
-                response.writeHead(404, {'Content-Type': 'text/html'});
-                return response.end("404 Not Found");
-            }
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(fileData);
-            return response.end();
-        });
-}
 
-socket_io.sockets.on(
+socket_io.on(
     'connection',
     function (socket)
     {
         socket.on(
-            'UpdateRGB',
+            'UpdateLamp',
             function(data)
             {
                 updateDevice(data.id, data.mode, data.R, data.G, data.B);
             });
     });
-
-http.listen(8080);
 
 process.on(
     'SIGINT',
