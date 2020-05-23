41a79a0974739b84a10afa21b1dbb3c2134ca237
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2020-05-23 13:39:13 +0200

    Separated service MDP (rgb & modbus master) requests.

--- ctrl.js
+++ ctrl.js
@@ -4,12 +4,46 @@ var socket_io = require('socket.io')(8090);
 
 const zmq = require("zeromq")
 
-async function updateDevice(id, mode, r, g, b)
+async function updateDevice(data)
 {
-    console.log("updateDevice", id, mode, r, g, b);
+    /* data format
+     * {
+     *     "id" : device_id,
+     *     "service" : name of MDP service able to process the payload
+     *     "payload" : [] - array of data
+     * }
+     * */
+        zmq_socket = new zmq.Request
+        zmq_socket.connect(BROKER_ENDPOINT);
+
+        console.log(
+            arguments.callee.name,
+            "id", data.id,
+            "service", data.service,
+            "payload length", data.payload.length);
+
+        await zmq_socket.send(
+            [
+                "MDPC01", /* MDP client signature */
+                data.service, /* service name */
+                JSON.stringify(data.payload)
+            ]);
+        [signature, service, reply] = await zmq_socket.receive();
+
+        console.log(
+            arguments.callee.name,
+            signature.toString(),
+            service.toString(),
+            reply.toString());
+}
+
+async function dispatchUpdateLamp(id, mode, r, g, b)
+{
+    console.log("dispatchUpdateLamp", id, mode, r, g, b);
 
     zmq_socket = new zmq.Request
     zmq_socket.connect(BROKER_ENDPOINT);
+
     await zmq_socket.send(
         [
             "MDPC01", /* MDP client signature */
@@ -21,25 +55,19 @@ async function updateDevice(id, mode, r, g, b)
                     "RGB" : [r, g, b]
                 })
         ]);
-    [signature, serivce, message] = await zmq_socket.receive();
 
-    console.log(signature.toString());
-    console.log(serivce.toString());
+    [signature, service, reply] = await zmq_socket.receive();
 
-    var jsonArray = JSON.parse(message.toString());
+    console.log(
+        arguments.callee.name,
+        signature.toString(),
+        service.toString());
 
-    console.log("data length ", jsonArray.length);
+    var jsonArray = JSON.parse(reply.toString());
 
     for(var i = 0; i < jsonArray.length; ++i)
     {
-        console.log(jsonArray[i]);
-
-        await zmq_socket.send(
-            [
-                "MDPC01", /* MDP client signature */
-                "modbus_master_/dev/ttyUSB0", /* service name */
-                JSON.stringify(jsonArray[i])
-            ]);
+        await updateDevice(jsonArray[i]);
     }
 }
 
@@ -52,7 +80,7 @@ socket_io.on(
             'UpdateLamp',
             function(data)
             {
-                updateDevice(data.id, data.mode, data.R, data.G, data.B);
+                dispatchUpdateLamp(data.id, data.mode, data.R, data.G, data.B);
             });
     });
 
