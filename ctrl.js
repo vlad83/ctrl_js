8ea1f0ee5e943ab2be1bd1735be1e57fd1648310
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2020-05-24 13:38:43 +0200

    Refactoring. Added error handling to ZMQ calls.

--- ctrl.js
+++ ctrl.js
@@ -1,8 +1,16 @@
 const BROKER_ENDPOINT = "tcp://127.0.0.1:6060"
+const MDP_RECV_TIMEOUT = 1000;
+const MDP_SEND_TIMEOUT = 1000;
 
-var socket_io = require('socket.io')(8090);
+let socket_io = require('socket.io')(8090);
 
 const zmq = require("zeromq")
+
+function log()
+{
+    console.log(new Date().toJSON(), arguments.callee.caller.name);
+    console.log.apply(console.log, arguments);
+}
 
 async function updateDevice(data)
 {
@@ -13,37 +21,54 @@ async function updateDevice(data)
      *     "payload" : [] - array of data
      * }
      * */
-        let zmq_socket = new zmq.Request
-        zmq_socket.connect(BROKER_ENDPOINT);
+    let zmq_socket =
+        new zmq.Request(
+        {
+            sendTimeout : MDP_SEND_TIMEOUT,
+            receiveTimeout : MDP_RECV_TIMEOUT
+        });
 
-        console.log(
-            arguments.callee.name,
-            "id", data.id,
-            "service", data.service,
-            "payload length", data.payload.length);
+    zmq_socket.connect(BROKER_ENDPOINT);
 
+    log(
+        "id", data.id,
+        "service", data.service,
+        "payload length", data.payload.length);
+
+    try
+    {
         await zmq_socket.send(
             [
                 "MDPC01", /* MDP client signature */
                 data.service, /* service name */
                 JSON.stringify(data.payload)
             ]);
+
         [signature, service, reply] = await zmq_socket.receive();
 
-        console.log(
-            arguments.callee.name,
+        log(
             signature.toString(),
             service.toString(),
             reply.toString());
+    }
+    catch(err)
+    {
+        log(err);
+        zmq_socket.disconnect(BROKER_ENDPOINT);
+    }
 }
 
 async function dispatchUpdateLamp(request)
 {
-    console.log(
-        arguments.callee.name,
-        request);
+    log("request", request);
 
-    let zmq_socket = new zmq.Request
+    let zmq_socket =
+        new zmq.Request(
+        {
+            sendTimeout : MDP_SEND_TIMEOUT,
+            receiveTimeout : MDP_RECV_TIMEOUT
+        });
+
     zmq_socket.connect(BROKER_ENDPOINT);
 
     let payload =
@@ -57,28 +82,34 @@ async function dispatchUpdateLamp(request)
         payload.RGB = [request.R, request.G, request.B];
     }
 
-    await zmq_socket.send(
-        [
-            "MDPC01", /* MDP client signature */
-            "rgb", /* service name */
-            JSON.stringify(payload)
-        ]);
-
-    [signature, service, reply] = await zmq_socket.receive();
-
-    console.log(
-        arguments.callee.name,
-        signature.toString(),
-        service.toString());
-
-    var jsonArray = JSON.parse(reply.toString());
-
-    for(var i = 0; i < jsonArray.length; ++i)
+    try
     {
-        await updateDevice(jsonArray[i]);
+        await zmq_socket.send(
+            [
+                "MDPC01", /* MDP client signature */
+                "rgb", /* service name */
+                JSON.stringify(payload)
+            ]);
+
+
+        [signature, service, reply] = await zmq_socket.receive();
+
+        if("service unsupported" !== reply.toString())
+        {
+            let jsonArray = JSON.parse(reply.toString());
+
+            for(let i = 0; i < jsonArray.length; ++i)
+            {
+                await updateDevice(jsonArray[i]);
+            }
+        }
+    }
+    catch(err)
+    {
+        log("error", err);
+        zmq_socket.disconnect(BROKER_ENDPOINT);
     }
 }
-
 
 socket_io.on(
     'connection',
